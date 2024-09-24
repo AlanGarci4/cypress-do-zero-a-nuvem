aaca09588467422a642fa153858177849ea1f286
AlanGarci4 <alangarciasantos@outlook.com>
2024-09-24 15:03:25 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: "6y773k",
+  projectId: "e4aq1t",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
